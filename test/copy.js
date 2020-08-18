db619336f46fed17ed6159f38e8b2930a3f25a4a
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2020-08-18 11:05:18 +0000

    fix: Change files path

--- test/copy.js
+++
@@ -1,9 +0,0 @@
-// Copy Text File
-const {
-  promises: { copyFile },
-} = require('fs');
-const { join } = require('path');
-
-copyFile(join(__dirname, 'mit.txt'), '.', (err) => {
-  if (err) console.log(err);
-}).catch((error) => console.log(error));
